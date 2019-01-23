6fe30dbf5bc8878891cf635be3d6ae3f9ad5b216
Denis Kakačka <dkakacka@sygic.com>
2019-01-23 15:10:46 +0100

    Initial Commit

---
+++ Showcase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Showcase
+//
+//  Created by Denis Kakačka on 23/01/2019.
+//  Copyright © 2019 Denis Kakačka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
